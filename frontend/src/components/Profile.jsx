c650adc88690ff964a41143d94bed3fd2e86d408
Abhishek Goyal <agoyal0308@gmail.com>
2024-08-02 17:45:09 +0530

    update error

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,10 +5,10 @@ import { Button } from './ui/button'
 import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
-import AppliedJobTable from './AppliedJobTable'
 import UpdateProfileDialog from './UpdateProfileDialog'
 import { useSelector } from 'react-redux'
 import useGetAppliedJobs from '@/hooks/useGetAppliedJobs'
+import AppliedJobTable from './AppliedJobTable'
 
 // const skills = ["Html", "Css", "Javascript", "Reactjs"]
 const isResume = true;
