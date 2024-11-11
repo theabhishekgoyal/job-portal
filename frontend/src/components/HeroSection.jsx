9812c9dcc482db64166ee8c43d106a0b5aacfb08
Abhishek Goyal <agoyal0308@gmail.com>
2024-11-12 00:05:45 +0530

    update in home page

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -18,9 +18,8 @@ const HeroSection = () => {
     return (
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 CareerCue Website</span>
-                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur temporibus nihil tempora dolor!</p>
+                <h1 className='text-5xl font-bold'>Find, Apply, and Land<br /> Your <span className='text-[#6A38C2]'>Perfect Job Opportunity!</span></h1>
+                <p>Discover and apply for job opportunities effortlessly, connecting students with recruiters for a streamlined hiring experience.</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
